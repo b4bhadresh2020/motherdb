e0c3dcd06694a6b1f476ebe7bd69d146e6ff67d4
darshana_boghara <darshana.codexive@gmail.com>
2021-07-05 16:04:04 +0530

    clever reach unsubscribe motherdb

--- application/controllers/MailUnsubscribe.php
+++ application/controllers/MailUnsubscribe.php
@@ -642,6 +642,72 @@ class MailUnsubscribe extends CI_Controller
                         ManageData(PROVIDER_UNSUBSCRIBER,[],$data,true);
                     }
                 }               
+            } else if($provider == CLEVER_REACH){
+                $this->load->model('mdl_clever_reach_unsubscribe');
+                //LIST ID EMPTY GET COUNTRY WISE LIST
+                $list = $this->input->post('list');
+                if(empty($list)){
+                    $listCondition  = array(
+                        'provider' => $provider,
+                        'clever_reach_accounts.status' => 1
+                    );
+                    if(!empty($country)) {
+                        $listCondition['country'] = $country;
+                    }
+                    $is_single             = false;
+                    // $getListIDByCountry    = GetAllRecord(PROVIDERS, $listCondition, $is_single,[],[],[],'id');
+                    $getListIDByCountry = JoinData(PROVIDERS,$listCondition,CLEVER_REACH_ACCOUNTS,"aweber_account","id","left",$is_single,array(),"providers.id","");
+                    $list = array_column($getListIDByCountry,'id');
+                }
+                
+                foreach ($list as $listID) {   
+                    
+                    // fetch mail provider data from providers table
+                    $providerCondition   = array('id' => $listID);
+                    $is_single           = true;
+                    $providerData        = GetAllRecord(PROVIDERS, $providerCondition, $is_single);
+
+                    // CHECK EMAIL ALREADY UNSUBSCRIBE
+                    if(!in_array($listID,$providerID)){
+                        // SEND DATA FOR UNSUBSCRIBE
+                        $response = $this->mdl_clever_reach_unsubscribe->makeUnsubscribe($email,$listID);
+
+                        // ADD RECORD IN DATABASE FOR UNSUBSCRIBER LIST.
+                        if($response["result"] == "success"){
+                            $data = [
+                                "provider_id" => $listID,
+                                "email"       => $email,
+                                "name"        => $response["data"]["name"],
+                                "status"      => 1, // success
+                                "response"    => $response["data"]["updated_at"]
+                            ];
+                            $successUnsubscribe[] = $providerData['listname'].'(Clever Reach)';
+                        }else{
+                            $data = [
+                                "provider_id" => $listID,
+                                "email"       => $email,
+                                "name"        => NULL,
+                                "status"      => 2, // error
+                                "response"    => $response["msg"]
+                            ];
+                            $failUnsubscribe[] = $providerData['listname'].'(Clever Reach)';
+                        }
+                        // INSERT DATA IN PROVIDER UNSUBSCRIBER TABLE
+                        ManageData(PROVIDER_UNSUBSCRIBER,[],$data,true);
+                    }else{
+                        $data = [
+                            "provider_id" => $listID,
+                            "email"       => $email,
+                            "name"        => NULL,
+                            "status"      => 3, // already unsubscribed
+                            "response"    => "Already unsubscribed"
+                        ];
+                        $alreadyUnsubscribe[] = $providerData['listname'].'(Clever Reach)';
+                        // INSERT DATA IN PROVIDER UNSUBSCRIBER TABLE
+                        ManageData(PROVIDER_UNSUBSCRIBER,[],$data,true);
+                    }
+                                    
+                }               
             }
         }
         $successUnsubscribeList = implode(", ",$successUnsubscribe);
