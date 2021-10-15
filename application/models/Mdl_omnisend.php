86797542c2716dbc3b60a0790dbeba0c4c93df13
jalpa <b4bhadresh2020@gmail.com>
2021-10-15 12:00:38 +0530

    omnisend gander bug fix

--- application/models/Mdl_omnisend.php
+++ application/models/Mdl_omnisend.php
@@ -60,9 +60,9 @@ class Mdl_omnisend extends CI_Model {
             $apiKey = $omnisendAccountData['api_key'];
 
             $gender = '';
-            if(!empty($getData['gender']) && strtolower($getData['gender'] == 'male')) {
+            if(!empty($getData['gender']) && strtolower($getData['gender'] == 'male') || strtolower($getData['gender'] == 'm')) {
                 $gender = 'm';
-            } else if (!empty($getData['gender']) && strtolower($getData['gender'] == 'female')) {
+            } else if (!empty($getData['gender']) && strtolower($getData['gender'] == 'female') || strtolower($getData['gender'] == 'f')) {
                 $gender = 'f';
             }
             $todayDateTime = date("Y-m-d\TH:i:s\Z", strtotime(date('Y-m-d h:i:s')));
